d999f9e0eca5cb1bafa3a9b00c4257be90450562
Matt Uttridge <matt@trazyinc.com>
2015-10-12 15:18:36 -0400

    Simplifying game class

--- src/game/game.js
+++ src/game/game.js
@@ -1,9 +1,5 @@
 import React from 'react/addons';
-import Debug from 'debug';
-
 import AppRoot from './components/AppRoot';
-
-var debug = Debug('nameGame');
 
 /*
  * @class Game
@@ -15,7 +11,6 @@ class Game {
    * @param {Object} options
    */
   constructor(options) {
-    debug('create name game with options', options);
     this.state = options.state;
   }
 
@@ -24,24 +19,11 @@ class Game {
    * @param {DOM} [element]
    * @returns {String|undefined}
    */
-   render (element) {
-    debug('render name game with state', this.state);
-
-    // would be in JSX: <AppRoot state={this.state} />
+  render (element) {
     var appRootElement = React.createElement(AppRoot, {
       state: this.state
     });
-
-    // render to DOM
-    if(element) {
-      debug('render to DOM');
-      React.render(appRootElement, element);
-      return;
-    }
-
-    // render to string
-    debug('render to string');
-    return React.renderToString(appRootElement);
+    React.render(appRootElement, element);
   }
 
   /*
@@ -49,20 +31,9 @@ class Game {
    * @param {DOM} element
    */
    renderToDOM (element) {
-    if(!element) {
-      return debug(new Error('App.renderToDOM: element is required'));
-    }
-
     this.render(element);
    }
 
-  /*
-   * @method renderToString
-   * @returns {String}
-   */
-   renderToString () {
-    return this.render();
-  }
 }
 
 export default Game;
