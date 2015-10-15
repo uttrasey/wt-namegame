a2806fb5d147b13a314020d585c7a8acbbe790ab
Matt Uttridge <matt@trazyinc.com>
2015-10-15 11:46:47 -0400

    Removing some commented out old code

--- src/client/scripts/client.js
+++ src/client/scripts/client.js
@@ -5,15 +5,3 @@ import Game from '../../game';
 
 var gameNode = document.getElementById('game');
 ReactDOM.render(<Game url={config.apiUrl} />, gameNode);
-
-
-
-/*
- * @description async get the employee data then render the game
- */
-// request(config.apiUrl, function (error, response, body) {
-//     if (!error && response.statusCode === 200) {
-//       var employees = JSON.parse(body);
-//       renderGame(employees);
-//     }
-// });
