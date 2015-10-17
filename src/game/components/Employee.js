7af7f6e7f7ba89da8e46e57c85d6fabe03435bba
Matt Uttridge <matt@trazyinc.com>
2015-10-17 16:01:43 -0400

    Refactoring to use iteration rather than hard coded

--- src/game/components/Employee.js
+++ src/game/components/Employee.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import classnames from 'classnames'
 import Image from 'react-bootstrap/lib/Image';
 import Thumbnail from 'react-bootstrap/lib/Thumbnail';
 
@@ -9,31 +8,26 @@ import Thumbnail from 'react-bootstrap/lib/Thumbnail';
  */
 class Employee extends React.Component {
 
-  /**
-   * @constructor
-   */
-  constructor (options) {
-    super(options);
-  }
-
   /*
    * @method render
    * @returns {JSX}
    */
   render () {
-    var classes = classnames('canfade', { 'fade': this.props.fade });
     return <Thumbnail style={{'display': 'inline-block'}}
                       src={this.props.employee.url}>
-             <h3 className={classes}>{this.props.employee.name}</h3>
+               {(() => {
+                  if (this.props.showName) {
+                    return <h3>{this.props.employee.name}</h3>;
+                  }
+               })()}
            </Thumbnail>;
   }
 }
 
 Employee.propTypes = {
-  employee: React.PropTypes.object.isRequired,
-  fade: React.PropTypes.bool
+  employee: React.PropTypes.object.isRequired
 };
 
-Employee.defaultProps = { fade: false };
+Employee.defaultProps = { showName: true };
 
 export default Employee;
--- src/game/components/NameGameBoard.js
+++ src/game/components/NameGameBoard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classnames from 'classnames';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
@@ -13,33 +14,68 @@ class NameGameBoard extends React.Component {
   constructor (options) {
     super(options);
     this.state = {
+      questionTime: false,
       fade: false
     };
   }
 
   componentDidMount () {
+    // fade the faces out
     setTimeout(() => {
-      this.hideFaces();
-    }, 2200);
-  }
+      this.setState({
+        fade: true
+      });
+    }, 2500);
 
-  hideFaces() {
-    this.setState({
-      fade: true
-    });
+    // present the question
+    setTimeout(() => {
+      this.setState({
+        questionTime: true
+      });
+    }, 4500);
   }
 
   /*
    * @method render
    * TODO: loop through props.employees
+   * TODO: always render the main div here (ternary immediately invoked)
    * @returns {JSX}
    */
   render () {
+    var content = this.state.questionTime ? this.presentQuestion() : this.presentEmployees();
     return <div className='nameGameBoard'>
-              <Employee key={1} employee={this.props.employees[0]} fade={this.state.fade} />
-              <Employee key={2} employee={this.props.employees[1]} fade={this.state.fade} />
-              <Employee key={3} employee={this.props.employees[2]} fade={this.state.fade} />
-           </div>
+            {content}
+          </div>;
+  }
+
+  presentEmployees () {
+    var classes = classnames('canfade', { 'fade': this.state.fade });
+    return <div className={classes}>
+              <div>
+                {this.props.employees.map(function(employee, i) {
+                  return (
+                    <Employee key={i} employee={employee} />
+                  );
+                })}
+              </div>
+              <div>
+                <h4>Get remembering!</h4>
+              </div>
+           </div>;
+  }
+
+  presentQuestion () {
+    var chosenEmployee = Math.floor(Math.random() * 3);
+    return <div>
+              <div>
+                <Employee key={1} employee={this.props.employees[1]} showName={false} />
+                <Employee key={2} employee={this.props.employees[2]} showName={false} />
+                <Employee key={3} employee={this.props.employees[0]} showName={false} />
+              </div>
+              <div>
+                <h4>Click on <b>{this.props.employees[2].name}</b></h4>
+              </div>
+          </div>;
   }
 
 }
