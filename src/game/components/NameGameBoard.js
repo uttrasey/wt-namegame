de5a5cff7bd66205ff6e2b5b88899c17aba492bb
Matt Uttridge <matt@trazyinc.com>
2015-10-17 23:31:19 -0400

    Refactor to GameBoard

--- src/game/components/NameGameBoard.js
+++ src/game/components/NameGameBoard.js
@@ -4,19 +4,22 @@ import shuffler from 'knuth-shuffle';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
-import Employee from './Employee';
 import Selectable from './Selectable';
+import Employee from './Employee';
 
 /*
- * @class NameGameBoard
+ * @class NameGameBoard representing a round of the game
  * @extends React.Component
  */
 class NameGameBoard extends React.Component {
 
+  /**
+   * @constructor
+   */
   constructor (options) {
     super(options);
     this.state = {
-      phase: 1, //1 is show, 2 is question, 3 is result (this can be done better)
+      phase: 1,
       fade: false
     };
   }
@@ -40,7 +43,7 @@ class NameGameBoard extends React.Component {
       });
     }, 2500);
 
-    // present the question
+    // then present the question
     setTimeout(() => {
       this.setState({
         phase: 2
@@ -48,24 +51,41 @@ class NameGameBoard extends React.Component {
     }, 4500);
   }
 
+  /**
+   * @description callback for when an employee is selected
+   */
+  employeeSelected (selectedEmployeeIndex) {
+    var correct = selectedEmployeeIndex === this.state.correctAnswerIndex;
+
+    setTimeout(() => {
+      this.props.roundOver(correct);
+    }, 1000);
+
+    this.setState({
+      phase: 3,
+      userAnswer: selectedEmployeeIndex
+    });
+  }
+
   /*
    * @method render
    * @returns {JSX}
    */
   render () {
-    var content;
-    if (this.state.phase === 1) {
-      content = this.presentEmployees();
-    } else if (this.state.phase === 2) {
-      content = this.presentQuestion();
-    } else if (this.state.phase === 3) {
-      content = this.presentResult();
-    }
     return <div className='board'>
-            {content}
+              {(() => {
+                switch (this.state.phase) {
+                  case 1: return this.presentEmployees();
+                  case 2: return this.presentQuestion();
+                  case 3: return this.presentResult();
+                }
+              })()}
           </div>;
   }
 
+  /**
+   * @description present phase 1, employee information
+   */
   presentEmployees () {
     var classes = classnames('canfade', { 'fade': this.state.fade });
     return <div className={classes}>
@@ -82,21 +102,8 @@ class NameGameBoard extends React.Component {
            </div>;
   }
 
-  employeeSelected (selectedEmployeeIndex) {
-    var correct = selectedEmployeeIndex === this.state.correctAnswerIndex;
-    
-    setTimeout(() => {
-      this.props.roundOver(correct);
-    }, 1000);
-
-    this.setState({
-      phase: 3,
-      userAnswer: selectedEmployeeIndex
-    });
-  }
-
   /**
-   *
+   * @description present phase 2, the question
    */
   presentQuestion () {
     return <div>
@@ -120,6 +127,9 @@ class NameGameBoard extends React.Component {
            </div>;
   }
 
+  /**
+   * @description present phase 3, the result the users answer
+   */
   presentResult () {
     var result = (this.state.userAnswer === this.state.correctAnswerIndex);
     return <div>
